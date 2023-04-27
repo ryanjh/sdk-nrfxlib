6f47360980b0d71142d2a29642b329addcb45e9c
Artur Hadasz <artur.hadasz@nordicsemi.no>
2023-04-27 09:59:00 +0200

    nrf_802154: rev c601469f41ecc4546cd1feb04d49b611cc872161
    This commit updates revision of the nrf_802154 component.
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>

--- nrf_802154/internal/driver/src/nrf_802154_aes_ccm_acc_ccm.c
+++ nrf_802154/internal/driver/src/nrf_802154_aes_ccm_acc_ccm.c
@@ -11,7 +11,16 @@
 
 #if NRF_802154_ENCRYPTION_ENABLED && !NRF_802154_ENCRYPTION_ACCELERATOR_ECB
 
-#define CCM_PS_COMPLIANT 0 // Configure CCM in compliance with product specification. Does not support information elements.
+/** Configure CCM in compliance with product specification.
+ *  Does not support multiprotocol solutions.
+ *  Does not support information elements.
+ */
+#define CCM_PS_COMPLIANT  0
+
+#define CCM_ALEN_ATTR_ID  11
+#define CCM_MLEN_ATTR_ID  12
+#define CCM_ADATA_ATTR_ID 13
+#define CCM_MDATA_ATTR_ID 14
 
 #include <assert.h>
 #include <string.h>
@@ -54,8 +63,12 @@ typedef struct
 __ALIGN(8) static nrf_vdma_job_t m_in_job_list[5];  ///< CCM DMA input job list array.
 __ALIGN(8) static nrf_vdma_job_t m_out_job_list[5]; ///< CCM DMA output job list array.
 
-static ccm_params_t m_ccm_params;                   ///< CCM peripheral parameters.
-static bool         m_initialized;                  ///< Module init status.
+static bool             m_setup;                    ///< Was encryption setup
+static ccm_params_t     m_ccm_params;               ///< CCM peripheral parameters.
+static bool             m_initialized;              ///< Module init status.
+static nrf_ccm_config_t m_ccm_config;               ///< CCM configuration used during the next transmission.
+static uint32_t         m_key[4];                   ///< Key used during the next transmission.
+static uint32_t         m_nonce[4];                 ///< Nonce used during the next transmission.
 
 static void ccm_disable(void)
 {
@@ -106,15 +119,130 @@ static void u8_string_to_u32x4_big_endian(const uint8_t * p_input,
     }
 }
 
-static bool ccm_configure(const nrf_802154_aes_ccm_data_t * p_aes_ccm_data,
+static void ccm_peripheral_configure(void)
+{
+    nrf_802154_irq_clear_pending(nrfx_get_irq_number(NRF_802154_CCM_INSTANCE));
+    nrf_802154_irq_enable(nrfx_get_irq_number(NRF_802154_CCM_INSTANCE));
+
+    nrf_ccm_enable(NRF_802154_CCM_INSTANCE);
+    nrf_ccm_configure(NRF_802154_CCM_INSTANCE, &m_ccm_config);
+    nrf_ccm_key_set(NRF_802154_CCM_INSTANCE, m_key);
+    nrf_ccm_nonce_set(NRF_802154_CCM_INSTANCE, m_nonce);
+    nrf_ccm_in_ptr_set(NRF_802154_CCM_INSTANCE, m_in_job_list);
+    nrf_ccm_out_ptr_set(NRF_802154_CCM_INSTANCE, m_out_job_list);
+    nrf_ccm_event_clear(NRF_802154_CCM_INSTANCE, NRF_CCM_EVENT_ERROR);
+    nrf_ccm_event_clear(NRF_802154_CCM_INSTANCE, NRF_CCM_EVENT_END);
+    nrf_ccm_int_enable(NRF_802154_CCM_INSTANCE, NRF_CCM_INT_ERROR_MASK | NRF_CCM_INT_END_MASK);
+    nrf_ccm_adatamask_set(NRF_802154_CCM_INSTANCE, 0xff);
+}
+
+static void ppi_configure(void)
+{
+    uint32_t dppi_ch_to_enable = (1 << NRF_802154_DPPI_RADIO_DISABLED);
+
+    nrf_ppib_subscribe_set(PPIB_RAD,
+                           nrf_ppib_send_task_get(NRF_802154_DPPI_RADIO_DISABLED),
+                           NRF_802154_DPPI_RADIO_DISABLED);
+    nrf_ppib_publish_set(PPIB_CCM,
+                         nrf_ppib_receive_event_get(NRF_802154_DPPI_RADIO_DISABLED),
+                         NRF_802154_DPPI_RADIO_DISABLED);
+
+#if CCM_PS_COMPLIANT
+    nrf_ppib_subscribe_set(PPIB_RAD,
+                           nrf_ppib_send_task_get(NRF_802154_DPPI_RADIO_TXREADY),
+                           NRF_802154_DPPI_RADIO_TXREADY);
+    nrf_ppib_publish_set(PPIB_CCM,
+                         nrf_ppib_receive_event_get(NRF_802154_DPPI_RADIO_TXREADY),
+                         NRF_802154_DPPI_RADIO_TXREADY);
+
+    nrf_ccm_subscribe_set(NRF_802154_CCM_INSTANCE,
+                          NRF_CCM_TASK_START,
+                          NRF_802154_DPPI_RADIO_TXREADY);
+
+    dppi_ch_to_enable |= (1 << NRF_802154_DPPI_RADIO_TXREADY);
+#endif // CCM_PS_COMPLIANT
+
+    nrf_ccm_subscribe_set(NRF_802154_CCM_INSTANCE,
+                          NRF_CCM_TASK_STOP,
+                          NRF_802154_DPPI_RADIO_DISABLED);
+
+    nrf_dppi_channels_enable(DPPIC_CCM, dppi_ch_to_enable);
+}
+
+static void ccm_configure(void)
+{
+    if (!m_initialized)
+    {
+        // DMASEC is set to non-secure by default, which prevents the CCM from accessing
+        // secure memory. Change the DMASEC to secure.
+#if defined(HALTIUM_XXAA)
+        nrf_spu_periph_perm_dmasec_set(NRF_SPU030, 10, true);
+#endif
+        nrf_802154_irq_init(nrfx_get_irq_number(NRF_802154_CCM_INSTANCE),
+                            NRF_802154_ECB_PRIORITY,
+                            ccm_irq_handler);
+        m_initialized = true;
+    }
+
+#if CCM_PS_COMPLIANT
+    ccm_peripheral_configure();
+    ppi_configure();
+#endif
+    m_setup = true;
+}
+
+static void vdma_jobs_fill(const nrf_802154_aes_ccm_data_t * p_aes_ccm_data,
+                           uint8_t                         * p_adata,
+                           uint8_t                         * p_mdata,
+                           uint8_t                           macsize)
+{
+    // CCM peripheral job list integer params pointed by p_job_ptr must have big-endian byte order.
+    host_16_to_little(p_aes_ccm_data->auth_data_len, (uint8_t *)&m_ccm_params.in.alen);
+    host_16_to_little(p_aes_ccm_data->plain_text_data_len, (uint8_t *)&m_ccm_params.in.mlen);
+
+    nrf_vdma_job_fill(&m_in_job_list[0],
+                      &m_ccm_params.in.alen,
+                      sizeof(m_ccm_params.in.alen),
+                      CCM_ALEN_ATTR_ID);
+    nrf_vdma_job_fill(&m_in_job_list[1],
+                      &m_ccm_params.in.mlen,
+                      sizeof(m_ccm_params.in.mlen),
+                      CCM_MLEN_ATTR_ID);
+    nrf_vdma_job_fill(&m_in_job_list[2],
+                      p_aes_ccm_data->auth_data,
+                      p_aes_ccm_data->auth_data_len,
+                      CCM_ADATA_ATTR_ID);
+    nrf_vdma_job_fill(&m_in_job_list[3],
+                      p_aes_ccm_data->plain_text_data,
+                      p_aes_ccm_data->plain_text_data_len,
+                      CCM_MDATA_ATTR_ID);
+    nrf_vdma_job_terminate(&m_in_job_list[4]);
+
+    nrf_vdma_job_fill(&m_out_job_list[0],
+                      &m_ccm_params.out.alen,
+                      sizeof(m_ccm_params.out.alen),
+                      CCM_ALEN_ATTR_ID);
+    nrf_vdma_job_fill(&m_out_job_list[1],
+                      &m_ccm_params.out.mlen,
+                      sizeof(m_ccm_params.out.mlen),
+                      CCM_MLEN_ATTR_ID);
+    nrf_vdma_job_fill(&m_out_job_list[2],
+                      p_adata,
+                      p_aes_ccm_data->auth_data_len,
+                      CCM_ADATA_ATTR_ID);
+    nrf_vdma_job_fill(&m_out_job_list[3],
+                      p_mdata,
+                      p_aes_ccm_data->plain_text_data_len + macsize,
+                      CCM_MDATA_ATTR_ID);
+    nrf_vdma_job_terminate(&m_out_job_list[4]);
+}
+
+static bool ccm_data_fill(const nrf_802154_aes_ccm_data_t * p_aes_ccm_data,
                           uint8_t                         * p_adata,
                           uint8_t                         * p_mdata)
 {
-    nrf_ccm_config_t ccm_config;
     nrf_ccm_maclen_t maclen;
     uint8_t          macsize;
-    uint32_t         key[4];
-    uint32_t         nonce[4];
 
     switch (p_aes_ccm_data->mic_level)
     {
@@ -140,101 +268,15 @@ static bool ccm_configure(const nrf_802154_aes_ccm_data_t * p_aes_ccm_data,
             return false;
     }
 
-    ccm_config.mode       = NRF_CCM_MODE_ENCRYPTION;
-    ccm_config.datarate   = NRF_CCM_DATARATE_250K;
-    ccm_config.protocol   = NRF_CCM_MODE_PROTOCOL_IEEE802154;
-    ccm_config.mac_length = maclen;
+    m_ccm_config.mode       = NRF_CCM_MODE_ENCRYPTION;
+    m_ccm_config.datarate   = NRF_CCM_DATARATE_250K;
+    m_ccm_config.protocol   = NRF_CCM_MODE_PROTOCOL_IEEE802154;
+    m_ccm_config.mac_length = maclen;
 
-    u8_string_to_u32x4_big_endian(p_aes_ccm_data->key, sizeof(p_aes_ccm_data->key), key);
-    u8_string_to_u32x4_big_endian(p_aes_ccm_data->nonce, sizeof(p_aes_ccm_data->nonce), nonce);
+    u8_string_to_u32x4_big_endian(p_aes_ccm_data->key, sizeof(p_aes_ccm_data->key), m_key);
+    u8_string_to_u32x4_big_endian(p_aes_ccm_data->nonce, sizeof(p_aes_ccm_data->nonce), m_nonce);
 
-    // CCM peripheral job list integer params pointed by p_job_ptr must have big-endian byte order.
-    host_16_to_little(p_aes_ccm_data->auth_data_len, (uint8_t *)&m_ccm_params.in.alen);
-    host_16_to_little(p_aes_ccm_data->plain_text_data_len, (uint8_t *)&m_ccm_params.in.mlen);
-
-    nrf_vdma_job_fill(&m_in_job_list[0], &m_ccm_params.in.alen, sizeof(m_ccm_params.in.alen), 11);
-    nrf_vdma_job_fill(&m_in_job_list[1], &m_ccm_params.in.mlen, sizeof(m_ccm_params.in.mlen), 12);
-    nrf_vdma_job_fill(&m_in_job_list[2],
-                      p_aes_ccm_data->auth_data,
-                      p_aes_ccm_data->auth_data_len,
-                      13);
-    nrf_vdma_job_fill(&m_in_job_list[3],
-                      p_aes_ccm_data->plain_text_data,
-                      p_aes_ccm_data->plain_text_data_len,
-                      14);
-    nrf_vdma_job_terminate(&m_in_job_list[4]);
-
-    nrf_vdma_job_fill(&m_out_job_list[0],
-                      &m_ccm_params.out.alen,
-                      sizeof(m_ccm_params.out.alen),
-                      11);
-    nrf_vdma_job_fill(&m_out_job_list[1],
-                      &m_ccm_params.out.mlen,
-                      sizeof(m_ccm_params.out.mlen),
-                      12);
-    nrf_vdma_job_fill(&m_out_job_list[2], p_adata, p_aes_ccm_data->auth_data_len, 13);
-    nrf_vdma_job_fill(&m_out_job_list[3],
-                      p_mdata,
-                      p_aes_ccm_data->plain_text_data_len + macsize,
-                      14);
-    nrf_vdma_job_terminate(&m_out_job_list[4]);
-
-    if (!m_initialized)
-    {
-        // DMASEC is set to non-secure by default, which prevents the CCM from accessing
-        // secure memory. Change the DMASEC to secure.
-#if defined(HALTIUM_XXAA)
-        nrf_spu_periph_perm_dmasec_set(NRF_SPU030, 10, true);
-#endif
-        nrf_802154_irq_init(nrfx_get_irq_number(NRF_802154_CCM_INSTANCE),
-                            NRF_802154_ECB_PRIORITY,
-                            ccm_irq_handler);
-        m_initialized = true;
-    }
-
-    nrf_802154_irq_clear_pending(nrfx_get_irq_number(NRF_802154_CCM_INSTANCE));
-    nrf_802154_irq_enable(nrfx_get_irq_number(NRF_802154_CCM_INSTANCE));
-
-    nrf_ccm_enable(NRF_802154_CCM_INSTANCE);
-    nrf_ccm_configure(NRF_802154_CCM_INSTANCE, &ccm_config);
-    nrf_ccm_key_set(NRF_802154_CCM_INSTANCE, key);
-    nrf_ccm_nonce_set(NRF_802154_CCM_INSTANCE, nonce);
-    nrf_ccm_in_ptr_set(NRF_802154_CCM_INSTANCE, m_in_job_list);
-    nrf_ccm_out_ptr_set(NRF_802154_CCM_INSTANCE, m_out_job_list);
-    nrf_ccm_event_clear(NRF_802154_CCM_INSTANCE, NRF_CCM_EVENT_ERROR);
-    nrf_ccm_event_clear(NRF_802154_CCM_INSTANCE, NRF_CCM_EVENT_END);
-    nrf_ccm_int_enable(NRF_802154_CCM_INSTANCE, NRF_CCM_INT_ERROR_MASK | NRF_CCM_INT_END_MASK);
-    nrf_ccm_adatamask_set(NRF_802154_CCM_INSTANCE, 0xff);
-
-    uint32_t dppi_ch_to_enable = (1 << NRF_802154_DPPI_RADIO_DISABLED);
-
-    nrf_ppib_subscribe_set(PPIB_RAD,
-                           nrf_ppib_send_task_get(NRF_802154_DPPI_RADIO_DISABLED),
-                           NRF_802154_DPPI_RADIO_DISABLED);
-    nrf_ppib_publish_set(PPIB_CCM,
-                         nrf_ppib_receive_event_get(NRF_802154_DPPI_RADIO_DISABLED),
-                         NRF_802154_DPPI_RADIO_DISABLED);
-
-#if CCM_PS_COMPLIANT
-    nrf_ppib_subscribe_set(PPIB_RAD,
-                           nrf_ppib_send_task_get(NRF_802154_DPPI_RADIO_TXREADY),
-                           NRF_802154_DPPI_RADIO_TXREADY);
-    nrf_ppib_publish_set(PPIB_CCM,
-                         nrf_ppib_receive_event_get(NRF_802154_DPPI_RADIO_TXREADY),
-                         NRF_802154_DPPI_RADIO_TXREADY);
-
-    nrf_ccm_subscribe_set(NRF_802154_CCM_INSTANCE,
-                          NRF_CCM_TASK_START,
-                          NRF_802154_DPPI_RADIO_TXREADY);
-
-    dppi_ch_to_enable |= (1 << NRF_802154_DPPI_RADIO_TXREADY);
-#endif
-
-    nrf_ccm_subscribe_set(NRF_802154_CCM_INSTANCE,
-                          NRF_CCM_TASK_STOP,
-                          NRF_802154_DPPI_RADIO_DISABLED);
-
-    nrf_dppi_channels_enable(DPPIC_CCM, dppi_ch_to_enable);
+    vdma_jobs_fill(p_aes_ccm_data, p_adata, p_mdata, macsize);
 
     return true;
 }
@@ -243,6 +285,8 @@ bool nrf_802154_aes_ccm_transform_prepare(const nrf_802154_aes_ccm_data_t * p_ae
 {
     uint8_t * p_work_buffer;
     uint8_t * p_ciphertext;
+
+    m_setup = false;
 
     // Verify that all necessary data is available
     if (p_aes_ccm_data->raw_frame == NULL)
@@ -288,12 +332,25 @@ bool nrf_802154_aes_ccm_transform_prepare(const nrf_802154_aes_ccm_data_t * p_ae
     memset(p_ciphertext, 0, p_aes_ccm_data->raw_frame[PHR_OFFSET] + PHR_SIZE - offset);
 #endif
 
-    return ccm_configure(p_aes_ccm_data, p_work_buffer + PHR_SIZE, p_ciphertext);
+    if (ccm_data_fill(p_aes_ccm_data, p_work_buffer + PHR_SIZE, p_ciphertext))
+    {
+        ccm_configure();
+        m_setup = true;
+    }
+
+    return m_setup;
 }
 
 void nrf_802154_aes_ccm_transform_start(uint8_t * p_frame)
 {
+    if (m_setup == false)
+    {
+        return;
+    }
 #if !CCM_PS_COMPLIANT
+    ccm_peripheral_configure();
+    ppi_configure();
+
     nrf_ccm_task_trigger(NRF_802154_CCM_INSTANCE, NRF_CCM_TASK_START);
 #endif
 }
@@ -301,11 +358,16 @@ void nrf_802154_aes_ccm_transform_start(uint8_t * p_frame)
 void nrf_802154_aes_ccm_transform_abort(uint8_t * p_frame)
 {
     ccm_disable();
+    m_setup = false;
+    memset(m_key, 0, sizeof(m_key));
+    memset(m_nonce, 0, sizeof(m_nonce));
 }
 
 void nrf_802154_aes_ccm_transform_reset(void)
 {
-    // Intentionally empty
+    m_setup = false;
+    memset(m_key, 0, sizeof(m_key));
+    memset(m_nonce, 0, sizeof(m_nonce));
 }
 
 #endif // NRF_802154_ENCRYPTION_ENABLED && !NRF_802154_ENCRYPTION_ACCELERATOR_ECB
